dde905c37ef2907b64b0872e782d0b625ce96418
Rohit Chand <chandrohitsumit@gmail.com>
2024-10-13 14:47:52 +0530

    Initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";  // Import path module
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -18,16 +19,30 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
 
-try {
-    mongoose.connect(URI);
-    console.log("Connected to MongoDB");
-} catch (error) {
-    console.log(error);
-}
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-//routes
+connectToDatabase();
+
+// routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+//--------------code for deployment
+if (process.env.NODE_ENV === "production") {
+    const dirPath = path.resolve();  // Use path.resolve correctly
+    
+    app.use(express.static("./Frontend/dist"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
